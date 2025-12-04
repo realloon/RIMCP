b840d95e9b638d1d483a51073d0da39637bf0269
sincerajn <sincerajn@outlook.com>
2025-12-04 19:37:00 +0800

    refactor: simplify judgment

--- src/utils/xml-utils.ts
+++ src/utils/xml-utils.ts
@@ -2,7 +2,13 @@ import { XMLParser, XMLBuilder } from 'fast-xml-parser'
 
 export const parser = new XMLParser({
   ignoreAttributes: false,
-  isArray: (tag, path) => tag === 'li' || tag === path.split('.').at(1),
+  isArray: (tag, path, _, isAttribute) => {
+    if (isAttribute) return false
+    if (tag === 'li') return true
+
+    const parts = path.split('.')
+    return parts.length === 2 && parts.at(0) === 'Defs'
+  },
 })
 
 export const builder = new XMLBuilder({
