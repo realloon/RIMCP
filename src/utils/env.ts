0510efe1021db347d7347ec5c911c3a8ffeea6f3
sincerajn <sincerajn@outlook.com>
2025-12-13 03:49:22 +0800

    fix: export root

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,6 +1,6 @@
 import { join } from 'path'
 
-const root = join(import.meta.dir, '../../')
+export const root = join(import.meta.dir, '../../')
 const distPath = join(root, 'dist')
 
 export const versionPath = join(distPath, 'Version.txt')
